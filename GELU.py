a8829785d7d8144b0a47c3bcce6cb641ab7ff432
william <liyijiang3000@gmail.com>
2025-05-19 18:54:28 -0700

    update template for webpage

--- GELU.py
+++
@@ -1,6 +0,0 @@
-from torch import nn
-import torch.nn.functional as F
-from torch import Tensor
-class GELU(nn.Module):
-    def forward(self, input: Tensor) -> Tensor:
-        return F.gelu(input)
--- hal/run.sh
+++
@@ -1,2 +0,0 @@
-python3 train_hal_node.py  --lr 1e-4  --lim 3  --nlr 0.1  --eps 0.01  --debug
-python3 train_hal_node.py  --lr 1e-4  --lim 3  --nlr 0.1  --eps 0.01  --no_adv  --debug
